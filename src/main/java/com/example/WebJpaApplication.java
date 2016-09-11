0b77b14c624f93c8c2ccbc91eeec61cd73e6cc91
lll <lisandrolazaro@gmail.com>
2016-09-10 23:55:53 -0400

    first commit

---
+++ src/main/java/com/example/WebJpaApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJpaApplication.class, args);
+	}
+}
