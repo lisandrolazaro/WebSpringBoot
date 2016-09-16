ab2627c43137aeb5077055d7be0fd115a4065cf2
lll25 <lisandro.lazaroluna@sky.uk>
2016-09-16 17:09:05 +0100

    insert controller for calling another service

---
+++ src/main/java/com/example/MainController.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.client.RestTemplate;
+/**
+ * Created by dev on 16/09/16.
+ */
+@Controller
+public class MainController {
+
+    @RequestMapping(value = "restCall" , method = RequestMethod.GET)
+    @ResponseBody
+    public String callSomeRestService() {
+        //return "Hello World";
+
+        RestTemplate rt = new RestTemplate();
+        rt.getMessageConverters().add(new StringHttpMessageConverter());
+
+        String uri = new String("someUrl");
+        return rt.getForObject(uri, String.class);
+
+    }
+}
--- src/main/java/com/example/WebJpaApplication.java
+++ src/main/java/com/example/WebJpaApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebJpaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebJpaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebJpaApplication.class, args);
+    }
 }
