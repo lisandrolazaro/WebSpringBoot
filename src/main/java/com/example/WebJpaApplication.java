f8fc65e326f661bd125840fd30f5748debb87c97
lll25 <lisandro.lazaroluna@sky.uk>
2016-09-19 17:29:23 +0100

    web application completed

--- src/main/java/com/example/WebJpaApplication.java
+++ src/main/java/com/example/WebJpaApplication.java
@@ -2,11 +2,18 @@ package com.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
-public class WebJpaApplication {
+public class WebJpaApplication extends WebMvcConfigurerAdapter {
 
     public static void main(String[] args) {
         SpringApplication.run(WebJpaApplication.class, args);
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "/readingList");
     }
 }
